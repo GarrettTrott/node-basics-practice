8f57a9abe8d8168fdf337c73b5b3d4075590e5fd
Garrett Trott <garretttrott@gmail.com>
2020-05-17 00:14:31 -0700

    adds more  error handling

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+// require https module
 const https = require("https");
+
+function printError(error) {
+  console.error(error.message);
+}
 
 //prints message to console
 function printMessage(username, badgeCount, points) {
@@ -12,28 +17,38 @@ function getProfile(username) {
     const request = https.get(
       `https://teamtreehouse.com/${username}.json`,
       (response) => {
-        let body = "";
-        // read the data
-        response.on("data", (data) => {
-          body += data.toString();
-        });
-        response.on("end", () => {
-          // parse the data
-          const profile = JSON.parse(body);
-          // print the data
-          printMessage(
-            username,
-            profile.badges.length,
-            profile.points.JavaScript
-          );
-        });
+        if (response.statusCode === 200) {
+          let body = "";
+          // read the data
+          response.on("data", (data) => {
+            body += data.toString();
+          });
+          response.on("end", () => {
+            try {
+              // parse the data
+              const profile = JSON.parse(body);
+              // print the data
+              printMessage(
+                username,
+                profile.badges.length,
+                profile.points.JavaScript
+              );
+            } catch (error) {
+              printError(error);
+            }
+          });
+        } else {
+          const message = `There was a problem getting the profile for ${username} (${response.statusCode})`;
+          const statusCodeError = new Error(message);
+          printError(statusCodeError);
+        }
       }
     );
     request.on("error", (error) =>
       console.error(`Problem with request: ${error.message}`)
     );
   } catch (error) {
-    console.error(error.message);
+    printError(error);
   }
 }
 const users = process.argv.slice(2);
