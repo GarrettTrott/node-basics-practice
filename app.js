752e7ac509600b7c4e7aa30809f9c88c6d571147
Garrett Trott <garretttrott@gmail.com>
2020-05-16 20:13:33 -0700

    add error handling

--- app.js
+++ app.js
@@ -7,27 +7,34 @@ function printMessage(username, badgeCount, points) {
 }
 
 function getProfile(username) {
-  // connect to the api url(http://teamtreehouse.com.garretttrott.json)
-  const request = https.get(
-    `https://teamtreehouse.com/${username}.json`,
-    (response) => {
-      let body = "";
-      // read the data
-      response.on("data", (data) => {
-        body += data.toString();
-      });
-      response.on("end", () => {
-        // parse the data
-        const profile = JSON.parse(body);
-        // print the data
-        printMessage(
-          username,
-          profile.badges.length,
-          profile.points.JavaScript
-        );
-      });
-    }
-  );
+  try {
+    // connect to the api url(http://teamtreehouse.com.garretttrott.json)
+    const request = https.get(
+      `https://teamtreehouse.com/${username}.json`,
+      (response) => {
+        let body = "";
+        // read the data
+        response.on("data", (data) => {
+          body += data.toString();
+        });
+        response.on("end", () => {
+          // parse the data
+          const profile = JSON.parse(body);
+          // print the data
+          printMessage(
+            username,
+            profile.badges.length,
+            profile.points.JavaScript
+          );
+        });
+      }
+    );
+    request.on("error", (error) =>
+      console.error(`Problem with request: ${error.message}`)
+    );
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 const users = process.argv.slice(2);
 
